ae4c16122438f430549448fe85d41e5bb1864940
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2021-09-03 16:11:35 +0300

    Fix deployment script

--- ops/scripts/deploy_app.sh
+++ ops/scripts/deploy_app.sh
@@ -2,5 +2,5 @@
 docker rm -f $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
 
-docker run -e RACK_ENV=production -e DATABASE_URL=${DATABASE_URL} ${RELEASE_IMAGE} bundle exec rake db:create db:migrate db:seed
-docker run -d -p 80:3000 --name app -e RACK_ENV=production -e DATABASE_URL=${DATABASE_URL} ${RELEASE_IMAGE} bundle exec rackup -p 3000
+docker run -e RACK_ENV=production --env DATABASE_URL ${RELEASE_IMAGE} bundle exec rake db:create db:migrate db:seed
+docker run -d -p 80:3000 --name app --env RACK_ENV=production --env DATABASE_URL ${RELEASE_IMAGE} bundle exec rackup -p 3000
