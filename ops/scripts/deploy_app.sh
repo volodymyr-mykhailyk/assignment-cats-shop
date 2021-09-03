d1b7acfd752e8e2aff7e81391fe9e6cb0ee5563d
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2021-09-03 15:28:37 +0300

    Fix artifcats producing

--- ops/scripts/deploy_app.sh
+++ ops/scripts/deploy_app.sh
@@ -1,2 +1,6 @@
-sudo docker run -e RACK_ENV=production -e DATABASE_URL=${DATABASE_URL} ${RELEASE_IMAGE} bundle exec rake db:create db:migrate db:seed
-sudo docker run -d -p 80:3000 --name app -e RACK_ENV=production -e DATABASE_URL=${DATABASE_URL} ${RELEASE_IMAGE} bundle exec rackup -p 3000
+#Clean up docker
+docker rm -f $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
+
+docker run -e RACK_ENV=production -e DATABASE_URL=${DATABASE_URL} ${RELEASE_IMAGE} bundle exec rake db:create db:migrate db:seed
+docker run -d -p 80:3000 --name app -e RACK_ENV=production -e DATABASE_URL=${DATABASE_URL} ${RELEASE_IMAGE} bundle exec rackup -p 3000
