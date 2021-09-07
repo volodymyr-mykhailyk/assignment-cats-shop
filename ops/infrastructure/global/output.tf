adec0cab9fcd0c6ca3ae5502bc6a7b255e9da470
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2021-09-07 10:34:12 +0300

    Output cidr block

--- ops/infrastructure/global/output.tf
+++ ops/infrastructure/global/output.tf
@@ -9,3 +9,7 @@ output "subnet_ids" {
 output "availability_zones" {
   value = module.vpc.availability_zones
 }
+
+output "cidr_block" {
+  value = module.vpc.cidr_block
+}
--- ops/infrastructure/modules/aws-eks-cluster/output.tf
+++ ops/infrastructure/modules/aws-eks-cluster/output.tf
@@ -3,5 +3,5 @@ output "cluster_name" {
 }
 
 output "cluster_ca_certificate" {
-  value = aws_eks_cluster.cluster.certificate_authority
+  value = aws_eks_cluster.cluster.certificate_authority[0].data
 }
