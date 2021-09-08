1cc3c2b1d76a016b9afab7dadefc8c415a4697ca
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2021-09-08 10:39:53 +0300

    Create k8s namespace

--- ops/infrastructure/pull-request/main.tf
+++ ops/infrastructure/pull-request/main.tf
@@ -11,14 +11,32 @@ data "terraform_remote_state" "global" {
     bucket = "terraform-state-517804334742"
     key    = "assignment-cats-shop/global.tfstate"
     region = "eu-central-1"
+  }
+}
 
-    dynamodb_table = "terraform-state-lock"
+data "terraform_remote_state" "live" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-state-517804334742"
+    key    = "assignment-cats-shop/live.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+provider "kubernetes" {
+  host                   = local.live_config["els_cluster_endpoint"]
+  cluster_ca_certificate = base64decode(local.live_config["eks_cluster_certificate"])
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", local.live_config["eks_cluster_name"]]
+    command     = "aws"
   }
 }
 
 locals {
   global_config = data.terraform_remote_state.global.outputs
-  name          = "${random_pet.shop_name.id}-shop"
+  live_config = data.terraform_remote_state.live.outputs
+  name          = "pr-${var.id}"
 }
 
 resource "random_pet" "shop_name" {
@@ -26,4 +44,10 @@ resource "random_pet" "shop_name" {
     id = var.id
   }
   separator = "-"
+}
+
+resource "kubernetes_namespace" "namespace" {
+  metadata {
+    name = local.name
+  }
 }
--- ops/infrastructure/pull-request/output.tf
+++ ops/infrastructure/pull-request/output.tf
@@ -1,3 +1,19 @@
 output "name" {
   value = local.name
 }
+
+output "namespace" {
+  value = kubernetes_namespace.namespace.metadata[0].name
+}
+
+output "ecr_repository_url" {
+  value = local.live_config["ecr_repository_url"]
+}
+
+output "eks_cluster_name" {
+  value = local.live_config["eks_cluster_name"]
+}
+
+output "database_url" {
+  value = ""
+}
