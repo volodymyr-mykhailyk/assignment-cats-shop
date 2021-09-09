44f335f364dc22803e9b6815ee42be55324dc7ce
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2021-09-09 11:31:40 +0300

    Increase default wait timeout

--- ops/scripts/wait_for_url.sh
+++ ops/scripts/wait_for_url.sh
@@ -3,7 +3,7 @@
 ATTEMPTS_COUNTER=0
 
 URL=$1
-MAX_ATTEMPTS=${2:-10}
+MAX_ATTEMPTS=${2:-30}
 
 until $(curl --output /dev/null --silent --head --fail ${URL}); do
     if [ ${ATTEMPTS_COUNTER} -eq ${MAX_ATTEMPTS} ];then
